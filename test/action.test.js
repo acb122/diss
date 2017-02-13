ccacedbeb769c1363e0ca0e376e51f64f7b51acc
Xander barnes <xandercb@hotmail.co.uk>
2017-02-13 13:28:57 +0000

    Merged PR 1: Merge boxSelection to master
    Related work items: #28

--- test/action.test.js
+++ test/action.test.js
@@ -63,4 +63,22 @@ describe('Action', () => {
         })
     })
 
+    describe('processImage', () => {
+        it('should return underfined if passed undefined', (done) => {
+            let imgData = actions.processImage(undefined)
+            assert.equal(imgData, undefined, 'not returning undefined')
+            done()
+        })
+
+        it('should split channels', (done) => {
+            actions.pixelResolution = 2
+            let data = {}
+            data.data = [1, 1, 3, 1, 1, 2, 3, 1, 1, 3, 3, 1, 1, 3, 3, 1]
+            let imgData = actions.processImage(data)
+            assert.deepEqual(imgData.red, [1, 1, 1, 1], 'not returning correct data')
+            assert.deepEqual(imgData.green, [1, 2, 3, 3], 'not returning correct data')
+            assert.deepEqual(imgData.blue, [3, 3, 3, 3], 'not returning correct data')
+            done()
+        })
+    })
 })
