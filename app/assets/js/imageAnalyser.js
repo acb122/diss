8985ea8035c750cec87c2c1afc2785c892ed38c1
acb122 <t-albar@microsoft.com>
2017-02-15 00:19:21 +0000

    calculateMean done needs tests still but its late

---
+++ app/assets/js/imageAnalyser.js
@@ -0,0 +1,62 @@
+class ImageAnalyser {
+
+  constructor(hrController, pixelResolution, imgData) {
+    this.hrController = hrController
+    this.pixelResolution = pixelResolution
+    this.rgb = this.processImage(imgData)
+    this.calculateMeans()
+  }
+
+  processImage(imgData) {
+    if (imgData) {
+      let red = []
+        , green = []
+        , blue = []
+        , alpha = []
+
+      for (let x = 0; x < this.pixelResolution * this.pixelResolution; x++) {
+        red[x] = imgData.data[0 + (4 * x)]
+        green[x] = imgData.data[1 + (4 * x)]
+        blue[x] = imgData.data[2 + (4 * x)]
+        alpha[x] = imgData.data[3 + (4 * x)]
+      }
+
+      return {
+        red: red
+        , green: green
+        , blue: blue
+        , alpha: alpha
+      }
+    } else {
+      return undefined
+    }
+  }
+
+  calculateMeans() {
+    for (var key in this.rgb) {
+      this.calculateMean(key)
+    }
+  }
+
+  calculateMean(key) {
+    if (key !== 'alpha') {
+      let previousAverage = this.hrController.getCurrentAverage(key)
+      let average = 0
+        , count = 0
+      this.rgb[key].forEach((value) => {
+        if ((previousAverage - 10 < value && value < previousAverage + 10) || previousAverage === 0) {
+          average += value
+          count++
+        }
+      }, this)
+      if (count === 0) {
+        console.log(22, average / count)
+        this.hrController.setCurrentAverage(key, 0)
+      } else {
+        console.log(average / count)
+        this.hrController.setCurrentAverage(key, average / count)
+      }
+    }
+  }
+}
+module.exports = ImageAnalyser
--- test/action.test.js
+++ test/action.test.js
@@ -60,25 +60,6 @@ describe('Action', () => {
     })
   })
 
-  describe('processImage', () => {
-    it('should return underfined if passed undefined', (done) => {
-      let imgData = actions.processImage(undefined)
-      assert.equal(imgData, undefined, 'not returning undefined')
-      done()
-    })
-
-    it('should split channels', (done) => {
-      actions.pixelResolution = 2
-      let data = {}
-      data.data = [1, 1, 3, 1, 1, 2, 3, 1, 1, 3, 3, 1, 1, 3, 3, 1]
-      let imgData = actions.processImage(data)
-      assert.deepEqual(imgData.red, [1, 1, 1, 1], 'not returning correct data')
-      assert.deepEqual(imgData.green, [1, 2, 3, 3], 'not returning correct data')
-      assert.deepEqual(imgData.blue, [3, 3, 3, 3], 'not returning correct data')
-      done()
-    })
-  })
-
   describe('stream', () => {
     it('make sure no error', (done) => {
       setupHTML()
@@ -158,25 +139,6 @@ describe('Action', () => {
         return
       }
       actions.loop()
-      done()
-    })
-  })
-
-  describe('processImage', () => {
-    it('should return underfined if passed undefined', (done) => {
-      let imgData = actions.processImage(undefined)
-      assert.equal(imgData, undefined, 'not returning undefined')
-      done()
-    })
-
-    it('should split channels', (done) => {
-      actions.pixelResolution = 2
-      let data = {}
-      data.data = [1, 1, 3, 1, 1, 2, 3, 1, 1, 3, 3, 1, 1, 3, 3, 1]
-      let imgData = actions.processImage(data)
-      assert.deepEqual(imgData.red, [1, 1, 1, 1], 'not returning correct data')
-      assert.deepEqual(imgData.green, [1, 2, 3, 3], 'not returning correct data')
-      assert.deepEqual(imgData.blue, [3, 3, 3, 3], 'not returning correct data')
       done()
     })
   })
